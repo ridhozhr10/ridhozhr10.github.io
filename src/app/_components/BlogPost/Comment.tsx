7d145fe9dcbd74a66c0e4475ff9bcd82ffaded1a
ridhozhr10 <azharridho42@gmail.com>
2024-05-21 14:48:31 +0700

    fix: comment not loaded on client side navigation

--- src/app/_components/BlogPost/Comment.tsx
+++ src/app/_components/BlogPost/Comment.tsx
@@ -1,30 +1,47 @@
+import { repoURL } from "@/constants";
 import Script from "next/script";
-import { RefObject } from "react";
+import { RefObject, useEffect, useState } from "react";
 
 type Props = {
   parentRef: RefObject<HTMLElement>;
 };
 
 export default function Comment({ parentRef }: Props) {
+  // rand based on time so script not getting cached on client side navigation
+  const [rand, setRand] = useState(0);
+  useEffect(() => {
+    setRand(new Date().getTime());
+
+    return () => {
+      setRand(0);
+    };
+  }, []);
+
+  if (rand === 0) {
+    return;
+  }
+
   return (
-    <Script
-      src="https://utteranc.es/client.js"
-      data-repo="ridhozhr10/ridhozhr10.github.io"
-      data-issue-term="url"
-      data-theme="github-dark"
-      data-crossorigin="anonymous"
-      data-async
-      async
-      onReady={() => {
-        const utteranceDOM = document.getElementsByClassName("utterances")[0];
-        if (!utteranceDOM) {
-          return;
-        }
-        // https://stackoverflow.com/a/33724397
-        const fragment = document.createDocumentFragment();
-        fragment.appendChild(utteranceDOM);
-        parentRef?.current?.appendChild(fragment);
-      }}
-    />
+    <>
+      <Script
+        src={`https://utteranc.es/client.js?rand=${rand}`}
+        data-repo={repoURL}
+        data-issue-term="url"
+        data-theme="github-dark"
+        data-crossorigin="anonymous"
+        data-async
+        async
+        onReady={() => {
+          const utteranceDOM = document.getElementsByClassName("utterances")[0];
+          if (!utteranceDOM) {
+            return;
+          }
+          // https://stackoverflow.com/a/33724397
+          const fragment = document.createDocumentFragment();
+          fragment.appendChild(utteranceDOM);
+          parentRef?.current?.appendChild(fragment);
+        }}
+      />
+    </>
   );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const baseURL = "https://ridhozhr10.github.io";
+export const repoURL = "ridhozhr10/ridhozhr10.github.io";
